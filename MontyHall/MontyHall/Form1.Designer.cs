f0a2467925f0f77527bc77648d888794d3113361
alecguarino97 <alecguarino97@gmail.com>
2018-09-16 17:09:19 -0400

    Initial commit

---
+++ MontyHall/MontyHall/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace MontyHall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.doorButton1 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.doorButton2 = new System.Windows.Forms.RadioButton();
+            this.doorButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.informationText = new System.Windows.Forms.TextBox();
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // doorButton1
+            // 
+            this.doorButton1.AutoSize = true;
+            this.doorButton1.Location = new System.Drawing.Point(3, 3);
+            this.doorButton1.Name = "doorButton1";
+            this.doorButton1.Size = new System.Drawing.Size(57, 17);
+            this.doorButton1.TabIndex = 3;
+            this.doorButton1.TabStop = true;
+            this.doorButton1.Text = "Door 1";
+            this.doorButton1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.doorButton3);
+            this.panel1.Controls.Add(this.doorButton2);
+            this.panel1.Controls.Add(this.doorButton1);
+            this.panel1.Location = new System.Drawing.Point(26, 107);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // doorButton2
+            // 
+            this.doorButton2.AutoSize = true;
+            this.doorButton2.Location = new System.Drawing.Point(4, 44);
+            this.doorButton2.Name = "doorButton2";
+            this.doorButton2.Size = new System.Drawing.Size(57, 17);
+            this.doorButton2.TabIndex = 4;
+            this.doorButton2.TabStop = true;
+            this.doorButton2.Text = "Door 2";
+            this.doorButton2.UseVisualStyleBackColor = true;
+            // 
+            // doorButton3
+            // 
+            this.doorButton3.AutoSize = true;
+            this.doorButton3.Location = new System.Drawing.Point(4, 80);
+            this.doorButton3.Name = "doorButton3";
+            this.doorButton3.Size = new System.Drawing.Size(57, 17);
+            this.doorButton3.TabIndex = 5;
+            this.doorButton3.TabStop = true;
+            this.doorButton3.Text = "Door 3";
+            this.doorButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Monty Hall Simulator";
+            // 
+            // informationText
+            // 
+            this.informationText.Location = new System.Drawing.Point(233, 107);
+            this.informationText.Multiline = true;
+            this.informationText.Name = "informationText";
+            this.informationText.ReadOnly = true;
+            this.informationText.Size = new System.Drawing.Size(245, 100);
+            this.informationText.TabIndex = 6;
+            this.informationText.TextChanged += new System.EventHandler(this.informationText_TextChanged);
+            // 
+            // acceptButton
+            // 
+            this.acceptButton.Location = new System.Drawing.Point(30, 214);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(196, 31);
+            this.acceptButton.TabIndex = 7;
+            this.acceptButton.Text = "Accept";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            this.acceptButton.Click += new System.EventHandler(this.acceptButton_Click);
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Location = new System.Drawing.Point(233, 214);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(245, 31);
+            this.RestartButton.TabIndex = 8;
+            this.RestartButton.Text = "Restart";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 426);
+            this.Controls.Add(this.RestartButton);
+            this.Controls.Add(this.acceptButton);
+            this.Controls.Add(this.informationText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Monty Hall Simulator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton doorButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton doorButton3;
+        private System.Windows.Forms.RadioButton doorButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox informationText;
+        private System.Windows.Forms.Button acceptButton;
+        private System.Windows.Forms.Button RestartButton;
+    }
+}
+
---
+++ MontyHall/MontyHall/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MontyHall
+{
+    public partial class Form1 : Form
+    {
+
+        bool[] doorHasCar = new bool[3];
+        bool guessedOnce;
+
+        public Form1()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+        private void NewGame() {
+            informationText.Text = "Welcome to the Monty Hall Simulator. " +
+                "There is a new car behind one of the 3 doors." +
+                "Guess which door it is & win yourself a new car!";
+
+            //Allows player to guess 2 times. 
+            guessedOnce = false;
+
+            //Allows player to guess
+            acceptButton.Enabled = true;
+
+            //Resets all doors to not have car
+            for (int i = 0; i < doorHasCar.Length; i++) {
+                 doorHasCar[i] = false;
+            }
+
+            //Sets 1 door to have car behind it.
+            Random rnd = new Random();
+            int highestIndexInArray = doorHasCar.Length - 1;
+            doorHasCar[rnd.Next(0, highestIndexInArray)] = true;
+        }
+
+        //Checks if player guessed the correct door. 
+        private bool CorrectChoice(int choice) {
+            choice -= 1;
+            if (doorHasCar[choice] == true) {
+                return true;
+            }
+            return false;
+        }
+
+
+
+        //Unused function. Do NOT DELETE. 
+        private void informationText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Restarts game
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void acceptButton_Click(object sender, EventArgs e)
+        {
+
+            bool GuessedCorrect = false;
+#region radio Button Check
+            if (doorButton1.Checked == true) {
+                GuessedCorrect = CorrectChoice(1);
+            }
+            else if (doorButton2.Checked == true) {
+                GuessedCorrect = CorrectChoice(2);
+            }
+            else if (doorButton3.Checked == true) {
+                GuessedCorrect = CorrectChoice(3);
+            }
+            #endregion
+
+            //Runs if the player guessed the right door and tells the player he wins. 
+            if (GuessedCorrect == true) {
+                informationText.Text = "You guessed correctly. You win! :D";
+                acceptButton.Enabled = false;
+                return;
+                
+            }
+
+            //Runs if this is the player's second guess and didn't get the answer correct. Ends the game. 
+            if (guessedOnce == true) {
+                int doorThatHasCar = -1;
+                for(int i = 0; i < doorHasCar.Length; i++) {
+                    if (doorHasCar[i] == true) {
+                        doorThatHasCar = (i + 1);
+                    }
+                }
+                informationText.Text = "Sorry. The car was behind door " + doorThatHasCar + ". Better luck next time! Click restart to try again.";
+                acceptButton.Enabled = false;
+                return;
+            }
+
+            //Runs if this is player's first guess & didn't get answer correct. 
+            informationText.Text = "Not Correct this time. I'll give you one last chance to get it right.";
+            guessedOnce = true;
+        }
+    }
+}
